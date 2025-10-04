2026f61d0e6f0434dcc4bdb6b1e5471881737c12
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-10-04 15:03:26 +0300

    cp

--- Levels/main.gd
+++ Levels/main.gd
@@ -7,6 +7,7 @@ const SLOT_POSITIONS := [
 ]
 
 @onready var taxes = $CanvasLayer/Taxes
+@onready var market = $Market
 
 func _ready() -> void:
     var all_buildings = BuildingData.get_all_buildings()
@@ -19,12 +20,49 @@ func _ready() -> void:
 
     taxes.taxes_set.connect(_on_taxes_set)
 
-func _on_taxes_set() -> void:
-    prints("taxes", TaxData.get_all_taxes()[0].value, TaxData.get_all_taxes()[1].value, TaxData.get_all_taxes()[2].value)
-    pass
-
 func place_building(building_data: BuildingData, slot_index: int) -> void:
     var building_instance = BUILDING_SCENE.instantiate()
     building_instance.building_name = building_data.building_name
     building_instance.position = SLOT_POSITIONS[slot_index]
     add_child(building_instance)
+
+func _on_taxes_set() -> void:
+    var buildings = get_tree().get_nodes_in_group("buildings")
+    var num_buildings = buildings.size()
+    var internal_demand := {
+        "Food": num_buildings,
+        "Clothes": num_buildings,
+        "Drink": num_buildings,
+        "Milk": 0,
+        "Meat": 0,
+        "Wool": 0,
+        "Sheep": 0
+    }
+
+    set_demand(internal_demand, buildings, "Food")
+    set_demand(internal_demand, buildings, "Clothes")
+    set_demand(internal_demand, buildings, "Drink")
+    set_demand(internal_demand, buildings, "Meat")
+    set_demand(internal_demand, buildings, "Milk")
+    set_demand(internal_demand, buildings, "Wool")
+   
+    for resource_name in ["Sheep", "Wool", "Milk", "Meat", "Food", "Clothes", "Drink"]:
+        var external_demand = market.demand_values.get(resource_name, 0)
+        var total_demand = int(internal_demand[resource_name] + external_demand)
+        prints(resource_name, " - Internal:", internal_demand[resource_name], " External:", external_demand, " Total:", total_demand)
+
+func set_demand(internal_demand: Dictionary, buildings: Array[Node], resource_name: String) -> void:
+    var producers: Array[BuildingData] = []
+    for building in buildings:
+        if building.building_data.output.resource_name == resource_name:
+            producers.append(building.building_data)
+
+    var demand = internal_demand[resource_name]
+    if producers.size() == 1:
+        internal_demand[producers[0].input.resource_name] += demand
+    elif producers.size() == 2:
+        var split_demand = demand / 2
+        internal_demand[producers[0].input.resource_name] += split_demand
+        internal_demand[producers[1].input.resource_name] += split_demand
+
+    prints(resource_name, internal_demand)
