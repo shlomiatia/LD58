2991cc9a03b6faad2d3203385f322807f895dfe3
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-10-04 12:40:14 +0300

    cp

--- Entities/Building/building.gd
+++ Entities/Building/building.gd
@@ -10,9 +10,19 @@ var building_data: BuildingData
 @onready var label = $Label
 @onready var conversion = $Conversion
 @onready var resource_price = $ResourcePrice
+@onready var sprite = $Sprite2D
+
+var is_hovering: bool = false
 
 func _ready() -> void:
     _update_building()
+
+func _process(_delta: float) -> void:
+    if building_data and building_data.input:
+        var mouse_pos = get_global_mouse_position()
+        var sprite_rect = Rect2(sprite.global_position - sprite.texture.get_size() / 2, sprite.texture.get_size())
+        is_hovering = sprite_rect.has_point(mouse_pos)
+        conversion.visible = is_hovering
 
 func _update_building() -> void:
     if building_name && is_node_ready():
@@ -23,9 +33,6 @@ func _update_building() -> void:
             if building_data.input:
                 conversion.input_resource_name = building_data.input.resource_name
                 conversion.output_resource_name = building_data.output.resource_name
-                conversion.visible = true
-            else:
-                conversion.visible = false
 
             if building_data.output:
                 resource_price.resource_name = building_data.output.resource_name
