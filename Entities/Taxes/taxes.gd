8a86e509abdfb0e07f8a6a871b79d061824e2008
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-10-05 19:33:48 +0300

    ccc

--- Entities/Taxes/taxes.gd
+++ Entities/Taxes/taxes.gd
@@ -1,31 +1,11 @@
-class_name Taxes extends HBoxContainer
+class_name Taxes extends VBoxContainer
 
 signal taxes_set
 
-@onready var tariff_tax = $VBoxContainer/Tariff
-@onready var vat_tax = $"VBoxContainer/VAT"
-@onready var income_tax = $"VBoxContainer/Income tax"
-@onready var set_button = $Button
-@onready var vbox = $VBoxContainer
-
-
-func _ready() -> void:
-    tariff_tax.tax_name = "Tariff"
-    vat_tax.tax_name = "VAT"
-    income_tax.tax_name = "Income tax"
-
-    set_controls_enabled(false)
-    set_button.pressed.connect(_on_set_button_pressed)
+var is_enabled: bool
 
 func _on_set_button_pressed() -> void:
-    set_controls_enabled(false)
     taxes_set.emit()
 
-func set_controls_enabled(enabled: bool) -> void:
-    for tax in vbox.get_children():
-        if tax is Tax:
-            tax.slider.editable = enabled
-    set_button.disabled = not enabled
-
 func are_controls_enabled() -> bool:
-    return set_button.disabled == false
+    return is_enabled
