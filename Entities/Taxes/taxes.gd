a9063e8571ef147ad557f74e5c8b4bb180a2f733
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-10-05 20:02:24 +0300

    aaa

--- Entities/Taxes/taxes.gd
+++ Entities/Taxes/taxes.gd
@@ -4,8 +4,8 @@ signal taxes_set
 
 var is_enabled: bool
 
-func _on_set_button_pressed() -> void:
-    taxes_set.emit()
+func set_controls_enabled() -> void:
+    is_enabled = true
 
 func are_controls_enabled() -> bool:
     return is_enabled
